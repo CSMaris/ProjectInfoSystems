f4561f27dacf6c08971abc7d42a635540eafbea9
CSMaris <catalinstefan.maris@studenti.polito.com>
2018-11-24 16:08:47 +0100

    Commit 24/11/2018 1

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, request, url_for, render_template, session, redirect
+from forms import loginForm, signupForm
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("layout.html")
+
+@app.route('/loginPage')
+def log():
+    form1=loginForm()
+    form2=signupForm()
+    #if form1.validate_on_submit():
+
+    #elif form2.validate_on_submit():
+
+
+
+    return "YOU LOGGED"
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, PasswordField, StringField
+from wtforms.validators import Email, DataRequired, Length
+
+class loginForm(FlaskForm):
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators= [DataRequired()])
+    submit=SubmitField('Log in')
+
+class signupForm(FlaskForm):
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Sign up')
