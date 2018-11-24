05fa4b8ba71e9c69f9b92057641f8b63b9db4c70
CSMaris <catalinstefan.maris@studenti.polito.com>
2018-11-24 20:57:53 +0100

    Commit 24/11/2018 2

--- app.py
+++ app.py
@@ -2,23 +2,29 @@ from flask import Flask, request, url_for, render_template, session, redirect
 from forms import loginForm, signupForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "vjhbgkyutgum"
 
 
 @app.route('/')
 def hello_world():
-    return render_template("layout.html")
+    return render_template("homeC.html")
 
-@app.route('/loginPage')
+@app.route('/loginPage', methods=['POST', 'GET'])
 def log():
     form1=loginForm()
     form2=signupForm()
-    #if form1.validate_on_submit():
+    namePage1=""
+    namePage2=""
+    if form1.validate_on_submit():
+        session['email1'] = form1.email.data
+        session['password1'] = form1.password.data
 
-    #elif form2.validate_on_submit():
+    if form2.validate_on_submit():
+        session['email2'] = form2.email.data
+        session['password2'] = form2.password.data
 
 
-
-    return "YOU LOGGED"
+    return render_template('login.html', form1=form1,form2=form2, namePage1=session.get('email1', False) )
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -2,10 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, PasswordField, StringField
 from wtforms.validators import Email, DataRequired, Length
 
+
 class loginForm(FlaskForm):
     email = StringField('Email:', validators=[DataRequired(), Email()])
-    password = PasswordField('Password',validators= [DataRequired()])
+    password = PasswordField('Password',validators= [DataRequired(), Length(min=8, max=20)])
     submit=SubmitField('Log in')
+
 
 class signupForm(FlaskForm):
     email = StringField('Email:', validators=[DataRequired(), Email()])
