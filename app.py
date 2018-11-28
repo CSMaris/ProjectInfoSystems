d91304a4a36af70dbd08af34f018adb0b9db9831
CSMaris <catalinstefan.maris@studenti.polito.com>
2018-11-28 11:20:07 +0100

    Commit 28/11/2018 1

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, url_for, render_template, session, redirect
 from forms import *
+import os
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "vjhbgkyutgum"
 
 
-@app.route('/', methods=['POST','GET'])
-def homeC():
-    formHomeC=searchCategoryForm()
-    return render_template('homeC.html', formHomeC=formHomeC)
+@app.route('/homec', methods=['POST','GET'])
+def homec():
+    formhomec=searchCategoryForm()
+    return render_template('homec.html', formhomec=formhomec)
 
 @app.route('/loginPage', methods=['POST', 'GET'])
 def log():
@@ -28,6 +30,44 @@ def signup():
         session['password2'] = form2.password.data
 
     return render_template('signup.html', form2=form2 )
+
+data = [
+    {
+        'title': 'product1',
+        'content':'Please Use Python 2.7.15 for Windows10',
+        'author': 'Mohammad',
+        'date': '14 Nov 2018'
+    },
+{
+        'title': 'Second Lesson',
+        'content':'Please Use Python 2.7.15 for Windows10',
+        'author': 'Mohammad',
+        'date': '12 Nov 2018'
+    },
+{
+        'title': 'Third Lesson',
+        'content':'Please Use Python 2.7.15 for Windows10',
+        'author': 'Mohammad',
+        'date': '10 Nov 2018'
+    }
+]
+
+
+
+
+@app.route('/',methods=['POST','GET'] )
+def products():
+ path='C:\Users\Stefan Maris\PycharmProjects\Project\static'
+ list = os.listdir(path)
+ images={}
+ for immagine in list:
+     nome=immagine.replace('.jpg', '')
+     images[nome]=(url_for('static', filename=immagine))
+
+
+ del(images['style.css'])
+
+ return render_template('productspage.html', immagini=images)
 
 
 if __name__ == '__main__':
